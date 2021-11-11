5ef6e7caf633d1d14046c862169fa037a8d43992
guyym <guyyomtov13@gmail.com>
2021-11-11 22:42:00 +0200

    initial check last one

--- Engine/src/Graph/Graph.java
+++ Engine/src/Graph/Graph.java
@@ -1,5 +1,21 @@
 package Graph;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Graph {
+    private Set<Targets> targets;
+    private boolean matrixOfDependency[][];
+
+    public void buildMe(File f)
+    {
+
+    }
+ /*   public Graph(Collection<Targets> info)
+    {
+        this.targets = new HashSet<Targets>(20);
+
+    }*/
 
 }

923e4c9c0f4506e9a4d5d2fff55fde56f3c0e1cb
heres <herespier@yahoo.com>
2021-11-12 18:45:03 +0200

    worked on function in BackDataManager

--- Engine/src/DataManager/BackDataManager.java
+++ Engine/src/DataManager/BackDataManager.java
@@ -5,10 +5,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 
 import Graph.*;
@@ -20,7 +17,7 @@ import javax.xml.bind.Unmarshaller;
 public class BackDataManager implements DataManager {
 
     private Graph graph;
-    private Map<String, Set<Target> mTypeToTargets = new HashMap<>();
+    private Map<String, Set<Target>> mTypeToTargets = new HashMap<String, Set<Target>>();
     private final static String JAXB_XML_GAME_PACKAGE_NAME = "resources";
 
     public boolean checkFile() throws ErrorUtils {
@@ -64,47 +61,94 @@ public class BackDataManager implements DataManager {
         }
 
     }
-    private Map<String, Set<Target>> makeMap(Set<Target> targets){
+    private Map<String, Set<Target>> makeMap(List<Target> targets){
 
+        String tType;
         Set<Target> leaf, independent, middle, root = new HashSet<Target>();
-        Map<String, Set<Target>> tmp = new HashMap<>();
 
+        Map<String, Set<Target>> tmp = new HashMap<>();
         tmp.put("Independent", new HashSet<Target>() );
         tmp.put("Leaf", new HashSet<Target>());
         tmp.put("Middle", new HashSet<Target>());
         tmp.put("Root", new HashSet<Target>());
 
+        for(Target curT : targets){
 
+            tType = (curT.getClass()).getSimpleName();
+
+            tmp.get(tType).add(curT);
+        }
         return tmp;
     }
+
     @Override
     public int getNumOfIndependents() {
-        return 0;
+        return this.graph.isGood = true ? (this.mTypeToTargets.get("Independent")).size() : -1;
     }
 
     @Override
     public int getNumOfRoots() {
-        return 0;
+        return this.graph.isGood = true ? (this.mTypeToTargets.get("Root")).size() : -1;
     }
 
     @Override
     public int getNumOfMiddle() {
-        return 0;
+        return this.graph.isGood = true ? (this.mTypeToTargets.get("Middle")).size() : -1;
     }
 
     @Override
     public int getNumOfLeafs() {
-        return 0;
+        return this.graph.isGood = true ? (this.mTypeToTargets.get("Leaf")).size() : -1;
     }
 
     @Override
     public int getNumOfTargets() {
-        return 0;
+        return this.graph.isGood = true ? (this.graph.getAllTargets()).size() : -1;
     }
 
     @Override
     public List<String> getInfoFromTarget(String nameOfTarget) throws ErrorUtils {
-        return null;
+
+        List<String> dataOfT = new ArrayList<>(5);
+
+        if(this.graph.isGood == false)
+            throw new ErrorUtils(ErrorUtils.noGraph());
+        else{
+
+            try {
+                Target target = this.graph.getThisTarget(nameOfTarget);
+
+                String listOfDependent = "", listOfRequired = "";
+
+                if(target.getClass().getSimpleName() == "Leaf"){
+                    Leaf leaf = (Leaf)target;
+                    listOfRequired = this.makeToString(leaf.getRequired());
+                }
+                else if(target.getClass().getSimpleName() == "Middle"){
+
+                    Middle middle = (Middle) target;
+                    listOfRequired = this.makeToString(middle.getRequired());
+                    listOfDependent = this.makeToString(middle.getDependencies());
+                }
+                else if(target.getClass().getSimpleName() == "Root"){
+
+                    Root root = (Root) target;
+                    listOfDependent = this.makeToString(root.middle.getDependencies());
+                }
+
+                dataOfT.add(0, target.getName());               // = target name;
+                dataOfT.add(1, target.getClass().getSimpleName());    // = location of the target.
+                dataOfT.add(2, listOfRequired);                 // = list of dependants targets
+                dataOfT.add(3, listOfDependent);                // = list of requires for targets.
+                dataOfT.add(4, target.getGenaralInfo());        // = general information.
+            }
+            catch(ErrorUtils e){ throw e;}
+        }
+        return dataOfT;
+    }
+
+    private String makeToString(List<Target> targets){
+
     }
 
     @Override

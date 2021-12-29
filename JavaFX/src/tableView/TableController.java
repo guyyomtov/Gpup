150fb41f7bc4b00eafa6880d6d510a15dc7c589a
guyym <guyyomtov13@gmail.com>
2021-12-29 23:31:19 +0200

    update ui

---
+++ JavaFX/src/tableView/TableController.java
@@ -0,0 +1,70 @@
+package tableView;
+
+import DataManager.BackDataManager;
+import Graph.Target;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class TableController {
+
+    @FXML
+    private TableView<Target> tableView;
+
+    @FXML
+    private TableColumn<Target, String> targetNameColumn;
+
+    @FXML
+    private TableColumn<Target, Target.Type> levelColumn;
+
+    @FXML
+    private TableColumn<Target, Integer> dependsOnColumn;
+
+    @FXML
+    private TableColumn<Target, Integer> requiredForColumn;
+
+    @FXML
+    private TableColumn<Target, String> infoColumn;
+
+    @FXML
+    private TableColumn<Target, Integer> serialSetColumn;
+
+
+
+    public void initTable(List<Target> targets) {
+        ObservableList<Target> data =
+                FXCollections.observableArrayList(targets);
+
+        targetNameColumn.setCellValueFactory(
+                new PropertyValueFactory<>("name")
+        );
+        levelColumn.setCellValueFactory(
+                new PropertyValueFactory<>("targetType")
+        );
+        dependsOnColumn.setCellValueFactory(
+                new PropertyValueFactory<>("totalDependsOn")
+        );
+        requiredForColumn.setCellValueFactory(
+                new PropertyValueFactory<>("totalRequiredFor")
+        );
+        infoColumn.setCellValueFactory(
+                new PropertyValueFactory<>("generalInfo")
+        );
+        serialSetColumn.setCellValueFactory(
+                new PropertyValueFactory<>("totalSerialNum")
+        );
+
+        tableView.setItems(data);
+    }
+
+
+
+}
---
+++ JavaFX/src/uploadFileView/MainController.java
@@ -0,0 +1,101 @@
+package uploadFileView;
+
+import DataManager.BackDataManager;
+import Mediator.UIDataManager;
+import errors.ErrorUtils;
+import graphInfoView.GraphInfoController;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import summaryForTable.SummeryForTableController;
+import tableView.TableController;
+
+import java.io.File;
+import java.io.IOException;
+
+
+public class MainController {
+
+    @FXML
+    private BorderPane MainBorderPane;
+
+    @FXML
+    private Button graphInfoButton;
+
+    @FXML
+    private Button InterrogatorButton;
+
+    @FXML
+    private Button taskButton;
+
+    @FXML
+    private MenuItem UploadFile;
+
+    @FXML
+    private ScrollPane welcomeView;
+
+    @FXML
+    private Label msgAfterUploadFile;
+
+    public static Stage primaryStage;
+
+    private BackDataManager bDM = new BackDataManager();
+
+
+    @FXML
+    void graphInfoAction(ActionEvent event){
+        try {
+            //FXMLLoader fxmlLoader = new FXMLLoader();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../graphInfoView/graphInfoFxml.fxml"));
+            GraphInfoController graphInfoController = loader.getController();
+            graphInfoController.setbDM(this.bDM);
+            graphInfoController.initGraphInfo();
+            Parent graphInfoView = loader.load();
+            int size = this.bDM.getAllTargets().size();
+            MainBorderPane.setCenter(graphInfoView);
+        }catch (IOException e) {
+           //e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void UploadFileAction(ActionEvent event) throws IOException {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("XML Files", "*.xml"));
+        File selectedFile = fileChooser.showOpenDialog(primaryStage);
+        String absolutePath = selectedFile.getAbsolutePath();
+        this.msgAfterUploadFile.setVisible(true);
+        try {
+            this.bDM.checkFile(absolutePath);
+        }catch (ErrorUtils e){
+             this.msgAfterUploadFile.setText(e.getMessage());
+             return;
+       }
+        this.graphInfoButton.setDisable(false);
+        this.InterrogatorButton.setDisable(false);
+        this.taskButton.setDisable(false);
+        this.msgAfterUploadFile.setText("The file " + selectedFile.getAbsolutePath() + " upload successfully");
+
+    //    this.msgAfterUploadFile.setVisible(true);
+
+    }
+
+    @FXML
+    void InterrogatorButtonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void taskButtonAction(ActionEvent event) {
+
+    }
+
+}

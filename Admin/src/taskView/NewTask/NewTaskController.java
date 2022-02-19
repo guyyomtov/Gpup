6ceef2380d68b6ee507da98e8462d264a20f4d51
heres <herespier@yahoo.com>
2022-02-19 15:04:29 +0200

    Made new UI task component

---
+++ Admin/src/taskView/NewTask/NewTaskController.java
@@ -0,0 +1,36 @@
+package taskView.NewTask;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+        import javafx.scene.control.Button;
+        import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import tableView.TableController;
+
+public class NewTaskController {
+
+    @FXML private GridPane gridPane;
+    @FXML private TableView targetTable;
+    @FXML private TableController targetTableController;
+    @FXML private TextField taskNameTextInput;
+    @FXML private RadioButton simulationRB;
+    @FXML private RadioButton compilationRB;
+    @FXML private Button sumbitButton;
+
+    @FXML void compilationRBOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void simulationRBOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sumbitButtonAction(ActionEvent event) {
+
+    }
+
+}
